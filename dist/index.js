8b5f01a9e6f56f955b33888fb2baea4868125098
CerBeer <ocelopp@gmail.com>
2024-10-27 01:56:15 +0300

    feat: create basic ws

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_js_1 = require("./src/http_server/index.js");
+const index_js_2 = require("./src/game_server/index.js");
 const HTTP_PORT = 8181;
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+const WS_PORT = 3000;
+(0, index_js_2.gameServer)(WS_PORT);
+console.log(`Start game server on the ${WS_PORT} port!`);
 index_js_1.httpServer.listen(HTTP_PORT);
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { httpServer } from './src/http_server/index.js';
+import { gameServer } from './src/game_server/index.js';
 
 const HTTP_PORT = 8181;
+const WS_PORT = 3000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+gameServer(WS_PORT);
+console.log(`Start game server on the ${WS_PORT} port!`);
+
 httpServer.listen(HTTP_PORT);
-
-console.log('\nBattleship is ready to play!');
-console.log('Go to: http://localhost:8181/\n');
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
